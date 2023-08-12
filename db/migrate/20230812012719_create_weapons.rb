ef85fb1432a7b15a71f8c740807a4ffacd952fe5
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-08-12 10:27:47 +0900

    add: weapon model

---
+++ db/migrate/20230812012719_create_weapons.rb
@@ -0,0 +1,11 @@
+class CreateWeapons < ActiveRecord::Migration[7.0]
+  def change
+    create_table :weapons do |t|
+      t.string :name
+      t.string :image_url
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
